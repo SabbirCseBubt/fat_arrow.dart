072c76a74c16faa273df1f56c466d75a48d866f4
syedhaque1122 <githubaayed@gmail.com>
2023-08-03 22:58:05 +0600

    Initial commit

---
+++ bin/fat_arrow.dart
@@ -0,0 +1,15 @@
+
+import 'package:fat_arrow/fat_arrow.dart' as fat_arrow;
+
+void main(List<String> arguments)
+{
+  func1();
+  print(add(10, 20));
+
+}
+
+
+void func1()=>print("Sabbir");
+
+
+int add(int a, int b)=> a+b;
